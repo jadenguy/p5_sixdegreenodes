253ba885cd4b12ca0c237e08e331376915506406
Alfredo Inostroza <jadenguy@gmail.com>
2022-01-11 14:29:56 -0800

    draw connections independantly

--- sketch.js
+++ sketch.js
@@ -1,52 +1,100 @@
-let field;
+let field, connections;
+const xCount = 10;
+
 function setup() {
   // console.log('setup');
   colorMode(HSB, 1);
   createCanvas(innerWidth, innerHeight);
   field = buildTable();
+  connections = buildConnections(field);
+  // console.table(field);
 }
 
 
 function draw() {
   console.log('step');
-  console.table(field);
   // console.log(new sixDegreeNode()+' ');
+  background(.75);
+  drawField(field);
+
+  drawConnections(connections);
   noLoop();
   // console.log('stepDone');
 }
 
-function drawField(eval = (point) => { return point.x > point.y }) {
+function drawField(points) {
+  fill(1)
+  noStroke();
+  let minAxis = Math.min(width, height);
+  let newScale = minAxis;
+  scale(newScale);
+  translate(.05, .05);
+  for (let index = 0; index < points.length; index++) {
+    const point = points[index];
+    circle(point.x, point.y, .01);
+  }
+}
 
+function drawConnections(links) {
+  stroke(0);
+  strokeWeight(.005);
+  const c = links.length;
+  for (let index = 0; index < c; index++) {
+    const link = links[index];
+    const a = link.left;
+    const b = link.right;
+    line(a.x, a.y, b.x, b.y);
+  }
 }
 
 
 function buildTable() {
-
   points = [];
-  for (let i = 0; i < 5; i++) {
-    points[i] = [];
-    for (let j = 0; j < 5; j++) {
-      points[i][j] = {
-        x: i,
-        y: j,
-        connections: []
+  for (let i = 0; i < xCount; i++) {
+    for (let j = 0; j < xCount; j++) {
+      const nodeObject = {
+        x: i / xCount,
+        y: j / xCount
       }
+      points.push(nodeObject);
     }
   }
   return points;
+}
+// function buildConnectionsFamily() {
+//   links = [];
+//   for (let i = 0; i < xCount; i++) {
+//     for (let j = 0; j < xCount; j++) {
+//       for (let k = j + 1; k < xCount; k++) {
+//         const connection = { left: i * xCount + j, right: i * xCount + k }
+//         links.push(connection);
+//       }
+//     }
+//   }
+//   return links;
+// }
+function buildConnections(points) {
+  links = [];
+  for (let i = 0; i < 10; i++) {
+    const l = floor(random(points.length));
+    const r = floor(l + random(points.length) + points.length - 1) % points.length;
+    const connection = { left: points[l], right: points[r] }
+    links.push(connection);
+  }
+  return links;
 }
 
 function mousePressed() { }
 
 function mouseReleased() {
-  // loop();
+  connections = buildConnections(field);
+  loop();
 }
 
 function mouseDragged() {
-  // decrementColor();
 }
+
 function windowResized() {
   resizeCanvas(innerWidth, innerHeight);
-  // field = buildTable();
   loop();
 }
